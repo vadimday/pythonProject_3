3c36c02ab1a026f966601ab7ff3e54457a0f2ff9
NN <asdzxc123fg@gmail.com>
2024-01-23 15:05:11 +0200

    HM ex 2 done

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
-
-input_string = input("Ряд: ")
-
-if input_string == input_string[::-1]:
-
- print("Пал+")
-
-else:
-
- print("Пал-")
-
-
 text = input("Текст: ")
 
-reserved_words = input("Слова через запятую: ").split(',')
+reserved_words = input("Слова через запятую: ").split(",")
 
+for word in reserved_words:
 
+   text = text.replace(word.strip(), word.strip().upper())
 
-for word in text.split():
-
-
-
- if word.lower() in reserved_words:
-
-   text = text.replace(word, word.upper())
-
-
+print(" текст:")
 
 print(text)
-
-
-
-text = input("Текст: ")
-
-
-
-num_sentences = text.count('.') + text.count('!') + text.count('?')
-
-
-
-print(f"Предложения в тексте: {num_sentences}")
-
